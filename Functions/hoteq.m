a34feb55cf8901509f0ab96cb83f15aae3b1a463
Guido Arnone <garnone@ws1.labo5>
2017-07-03 17:16:43 -0300

    Optimizamos el calculo de sistema llenando la matriz una unica vez. TODO: achicar la matriz sacando las filas de ceros que no dejan invertirla eficientemente

--- Functions/hoteq.m
+++ Functions/hoteq.m
@@ -26,8 +26,25 @@ function U = hoteq(alpha, h, dt, f_index)
     end 
   end
 
+ #Llenamos la matriz del sistema en base a los coeficientes
+ #anteriormente calculados.
+ for i = 1:N
+    for j = 1:N
+      for l = 1:(N^2)
+          A(coord(i,j,N), l) = 0; 
+      end
+      if i > 1 && i < N && j > 1 && j < N
+        A(coord(i,j,N),coord(i,j,N)) = (-1) -(4*mu);
+        A(coord(i,j,N),coord(i,j+1,N)) = mu;
+        A(coord(i,j,N),coord(i,j-1,N)) = mu;
+        A(coord(i,j,N),coord(i+1,j,N)) = mu;
+        A(coord(i,j,N),coord(i-1,j,N)) = mu;
+      end
+    end
+ end
+
  #Resolvemos para cada t un sistema lineal, en base a la
- #matriz anterior, basandonos en cierta relacion de
+ #solucion anterior del sistema basandonos en cierta relacion de
  #recurrencia.
  for k = 1:M
    lu = (-1)*u;
@@ -35,20 +52,6 @@ function U = hoteq(alpha, h, dt, f_index)
      for j = 1:N
        lu(coord(i,j, N)) -= dt*f(x(i),y(j),t(k), f_index);
      end 
-   end
-   for i = 1:N
-     for j = 1:N
-       for l = 1:(N^2)
-           A(coord(i,j,N), l) = 0; 
-       end
-       if i > 1 && i < N && j > 1 && j < N
-         A(coord(i,j,N),coord(i,j,N)) = (-1) -(4*mu);
-         A(coord(i,j,N),coord(i,j+1,N)) = mu;
-         A(coord(i,j,N),coord(i,j-1,N)) = mu;
-         A(coord(i,j,N),coord(i+1,j,N)) = mu;
-         A(coord(i,j,N),coord(i-1,j,N)) = mu;
-       end
-     end
    end
    u = (A\lu')';
    U = to_matrix(u,N);
